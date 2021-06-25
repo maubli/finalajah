b4d96421cc44feb7b799e2d95fed55c58305eff8
maubli <andhikade@gmail.com>
2021-06-25 20:09:52 +0700

    main

---
+++ www.suzuki.co.id/themes/mobile2019/js/app.minb6fc.js
@@ -0,0 +1,67 @@
+function openNav() {
+    $("#nav").addClass("open"), $("body").addClass("nav-opened")
+}
+
+function closeNav() {
+    $("#nav").removeClass("open"), $("body").removeClass("nav-opened")
+}
+
+function openSubNav(a) {
+    $("#nav-main").hide(), $(a).fadeIn()
+}
+
+function closeSubNav() {
+    $(".sub-nav").hide(), $("#nav-main").fadeIn()
+}
+jQuery(function(a) {
+    AOS.init({
+        offset: 300,
+        duration: 500
+    });
+    var e = a("header").height();
+    a(window).scroll(function() {
+        a(window).scrollTop() >= e ? a("header").addClass("header-solid") : a("header").removeClass("header-solid")
+        a(window).scrollTop() >= e ? $(".breadcrumb-2").removeClass('d-none') : $(".breadcrumb-2").addClass('d-none')
+    }), a(".main-slider").owlCarousel({
+        autoHeight: !0,
+        items: 1,
+        loop: !0,
+        margin: 0,
+        nav: !1,
+        animateOut: "fadeOut",
+        animateIn: "fadeIn"
+    })
+});
+
+
+// $('#show3d').click(function () {
+//   $('#loading').fadeIn();
+//   $('a.js-fullscreen').click(function(e) {
+//     e.preventDefault();
+//     $('.spritespin').spritespin('api').requestFullscreen();  
+//   });
+
+//   $(".spritespin").attr("style", "margin-left: -10% !important;");
+
+//   $('.spritespin').spritespin({
+//     source: SpriteSpin.sourceArray(source_image+'{frame}.png', { frame: [0,60], digits: 4 }),
+//     sense: -1,
+//     width: 400,
+//     height: 250,
+//     responsive: false,
+//     loop  : false, 
+//     animate  : false, 
+//   });
+
+//   $('#mainImage').hide();
+//   $('#threedContent').show();
+
+//   $('#hide3d').show();
+//   $('#loading').fadeOut();
+// });
+
+// $('#hide3d').click(function () {
+//   $('#threedContent').hide();
+//   $('#mainImage').show();
+//   $('#hide3d').hide();
+// });
